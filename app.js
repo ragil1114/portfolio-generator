f653cd0f4a9fccf9797a34c9a4238498ccd7951d
Ragil14 <rahil.gilani@gmail.com>
2022-01-11 17:02:21 -0600

    added comments & cleaned code

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
+// Notice the lack of parentheses around the `profileDataArr` parameter. That's cause when there's only one argument to pass, you can omit parenthases. 
 const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-      console.log(profileDataArr[i]);
-    }
+  /* This loop...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+  */
+
+  // just a divider
+  console.log('================');
+
+  /* ^Is the same as this array method...
+  profileDataArr.forEach((profileItem) => {
+    console.log(profileItem)
+  });
+  */
+
+  // ^& this array method cleaned up...
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
