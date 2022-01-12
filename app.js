f3340cc5dcf709f3ca98d6082d9a2b50138b5c0d
Ragil14 <rahil.gilani@gmail.com>
2022-01-12 13:56:06 -0600

    Used Template Literals inside generatePage() w/ Multi-Line Strings & combined two variables using Assignment Destructuring.

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+// Variable that holds the command-line arguments.
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-/*
+const [name, github] = profileDataArgs;
+/* ^^This combination aka Assignment Destructuring (assigns elements of an array to variable names in a single expression) can also be written as two separate variables...
+const name = profileDataArgs[0];
+const github = profileDataArgs[1];
+
 const generatePage = (username, githubName) => `Name: ${username}, Github: ${githubName}`;
 ^^You can also write the function above this way to create Multi-line Strings...
 */
@@ -12,4 +16,4 @@ const generatePage = (userName, githubName) => {
 };
 
 // Call
-console.log(generatePage('Jane', 'janehub'));
+console.log(generatePage(Name, github));
