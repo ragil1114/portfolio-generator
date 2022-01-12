24f20a3c0cae3cd12957845a3f110968df883d35
Ragil14 <rahil.gilani@gmail.com>
2022-01-12 16:21:28 -0600

    replaced function call & console.log() w/FS Module fs.writeFile

--- app.js
+++ app.js
@@ -23,6 +23,8 @@ const generatePage = (userName, githubName) => {
   `;
 };
 
-console.log(name, github);
-// Call
-console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
