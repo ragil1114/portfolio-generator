44d9582f910afb15687c5031224736c2d0a2353a
Ragil14 <rahil.gilani@gmail.com>
2022-01-12 13:33:00 -0600

    cleaned code

--- app.js
+++ app.js
@@ -1,29 +1,16 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
-/*
-// Notice the lack of parentheses around the `profileDataArr` parameter. That's cause when there's only one argument to pass, you can omit parenthases. 
-const printProfileData = profileDataArr => {
-  // This loop...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  // just a divider
-  console.log('================');
-
-  // ^Is the same as this array method...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
-
-  // ^& this array method cleaned up...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+/* Function:
+const generatePage = (username, githubName) => `Name: ${username}, Github: ${githubName}`;
+*/
+// ^^You can also write the function this way to create Multi-line Strings...
+const generatePage = (userName, githubName) => {
+  return `
+    Name: ${userName}
+    GitHub: ${githubName}
+  `;
 };
 
 // Call
-printProfileData(profileDataArgs);
-*/
-
-const generatePage = () => 'Name: Jane, Github: janehub';
-console.log(generatePage());
+console.log(generatePage('Jane', 'janehub'));
