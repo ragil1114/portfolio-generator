b07706672494bceb80235dec964248a783e27439
Ragil14 <rahil.gilani@gmail.com>
2022-01-13 15:55:56 -0600

    added inquirer variable & function to app.js file & commented out the previous code

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
+const inquirer = require('inquirer');
 // Global variable to allow fs module to work in the app.
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-// Variable that holds the command-line arguments.
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// Combined two variables w/Assignment Destructuring, which assigns elements of an array to variable names in a single expression.
-const [name, github] = profileDataArgs;
+//const fs = require('fs');
+//const generatePage = require('./src/page-template.js');
+
+//const pageHTML = generatePage(name, github);
 
 // FS Module that replaced the function call() & console.log().
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+//fs.writeFile('./index.html', pageHTML, err => {
+  //if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+  //console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
