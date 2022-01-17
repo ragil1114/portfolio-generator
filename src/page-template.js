4eadda154472f24222fedbe7419924d9be2992a1
Ragil14 <rahil.gilani@gmail.com>
2022-01-17 17:35:09 -0600

    created Project Section HTML

--- src/page-template.js
+++ src/page-template.js
@@ -12,6 +12,61 @@ const generateAbout = aboutText => {
   `;
 };
 
+// create the projects section
+const generateProjects = projectsArr => {
+  // get array of just featured projects
+  const featuredProjects = projectsArr.filter(project => {
+    if (project.feature) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+// Get array of all featured & non-featured projects using .filter() & .map() Array Methods.
+const generateProjects = projectsArr => {
+  return `
+    <section class="my-3" id="portfolio">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+      <div class="flex-row justify-space-between">
+      ${projectsArr
+        .filter(({ feature }) => feature)
+        .map(({ name, description, languages, link }) => {
+          return `
+          <div class="col-12 mb-2 bg-dark text-light p-3">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+        `;
+        })
+        .join('')}
+  
+      ${projectsArr
+        .filter(({ feature }) => !feature)
+        .map(({ name, description, languages, link }) => {
+          return `
+          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+            <h3 class="portfolio-item-title text-light">${name}</h3>
+            <h5 class="portfolio-languages">
+              Built With:
+              ${languages.join(', ')}
+            </h5>
+            <p>${description}</p>
+            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+          </div>
+        `;
+        })
+        .join('')}
+      </div>
+    </section>
+  `;
+};
+
 // Function to dynamically generate HTML file.
 module.exports = templateData => {
   //console.log(templateData);
@@ -31,7 +86,7 @@ module.exports = templateData => {
       <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="style.css">
     </head>
-  
+    
     <body>
       <header>
         <div class="container flex-row justify-space-between align-center py-3">
@@ -43,6 +98,7 @@ module.exports = templateData => {
       </header>
       <main class="container my-5">
         ${generateAbout(about)}
+        ${generateProjects(projects)}
       </main>
       <footer class="container text-center py-3">
         <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
@@ -50,6 +106,5 @@ module.exports = templateData => {
     </body>
     </html>
     `;
+  }
 };
-
-//module.exports = generatePage;
