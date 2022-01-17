6a07338f57db1086d7042233b1c509c1eb171690
Ragil14 <rahil.gilani@gmail.com>
2022-01-17 13:26:56 -0600

    validated answers by adding if/else statements & added When method to conditionally prompt a question based on user's input

--- app.js
+++ app.js
@@ -17,17 +17,49 @@ const inquirer = require('inquirer');
       {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
+        message: 'What is your name? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } 
+          else {
+            console.log('Please enter your name!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'github',
-        message: 'Enter your GitHub Username'
+        message: 'Enter your GitHub Username (Required)',
+        validate: githubInput => {
+          if (githubInput) {
+            return true;
+          } 
+          else {
+            console.log('Please enter your GitHub username!');
+            return false;
+          }
+        }
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAbout',
+        message: 'Would you like to enter some information about yourself for an "About" section?',
+        default: true
       },
       {
         type: 'input',
         name: 'about',
-        message: 'Provide some information about yourself:'
+        message: 'Provide some information about yourself:',
+        when: ({ confirmAbout }) => {
+          if (confirmAbout) {
+            return true;
+          } 
+          else {
+            return false;
+          }
+        }
       }
     ]);
   };
@@ -50,12 +82,30 @@ const inquirer = require('inquirer');
       {
         type: 'input',
         name: 'name',
-        message: 'What is the name of your project?'
+        message: 'What is the name of your project? (Required)',
+        validate: nameInput => {
+          if (nameInput) {
+            return true;
+          } 
+          else {
+            console.log('You need to enter a project name!');
+            return false;
+          }
+        }
       },
       {
         type: 'input',
         name: 'description',
-        message: 'Provide a description of the project. (Required)'
+        message: 'Provide a description of the project. (Required)',
+        validate: descriptionInput => {
+          if (descriptionInput) {
+            return true;
+          } 
+          else {
+            console.log('You need to enter a project description!');
+            return false;
+          }
+        }
       },
       {
         type: 'checkbox',
@@ -66,7 +116,16 @@ const inquirer = require('inquirer');
       {
         type: 'input',
         name: 'link',
-        message: 'Enter the GitHub link to your project. (Required)'
+        message: 'Enter the GitHub link to your project. (Required)',
+        validate: linkInput => {
+          if (linkInput) {
+            return true;
+          } 
+          else {
+            console.log('You need to enter a project GitHub link!');
+            return false;
+          }
+        }
       },
       {
         type: 'confirm',
