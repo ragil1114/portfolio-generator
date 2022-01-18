84abc3e389074d3ff4434dd4752857d9b4e9f99d
Ragil14 <rahil.gilani@gmail.com>
2022-01-17 20:36:25 -0600

    Copy a File using Promises

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
+const { resolve } = require('path');
 
+// writing file
 const writeFile = fileContent => {
     return new Promise((resolve, reject) => {
         fs.writeFile('./dist/index.html', fileContent, err => {
@@ -15,6 +17,23 @@ const writeFile = fileContent => {
               ok: true,
               message: 'File created!'
             });
-          });
+        });
+    });
+};
+
+// copying file
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'Stylesheet created!'
+            });
+        });
     });
 };
